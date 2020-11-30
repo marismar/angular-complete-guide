b1fa3c7543380dd67abc2ad71cd655f30b12e4de
Marismar Costa <cmarismar@gmail.com>
2020-11-30 00:33:51 -0300

    directives deep dive

---
+++ directives/src/app/app.module.ts
@@ -0,0 +1,13 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { BasicHighlightDirective } from './basic-highlight/basic-highlight.directives';
+
+@NgModule({
+  declarations: [AppComponent, BasicHighlightDirective],
+  imports: [BrowserModule],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
