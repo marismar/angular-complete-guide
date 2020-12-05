d8861ce0b3787e46efa40cad64a8568399d435e5
Marismar Costa <cmarismar@gmail.com>
2020-12-05 17:39:39 -0300

    handling template driven forms in angular

--- template-driven-form/src/app/app.component.ts
+++ template-driven-form/src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,9 +7,49 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
+  @ViewChild('form') form: NgForm;
   title = 'template-driven-form';
+  defaultOption = 'pet';
+  answer = '';
+  genders = ['male', 'female'];
+  submitted = false;
+  user = {
+    username: '',
+    email: '',
+    secret: '',
+    answer: '',
+    gender: '',
+  };
 
   suggestUserName(): void {
-    const suggestedName = 'Superuser';
+    const suggestedName = 'superuser';
+    // setValue() overwrite all inputs
+    /* this.form.setValue({
+      userData: {
+        username: suggestedName,
+        email: '',
+      },
+      secret: 'pet',
+      question: '',
+      gender: 'female',
+    }); */
+
+    // patchValue() dont overwrite filled inputs, but only empty one
+    this.form.form.patchValue({
+      userData: {
+        username: suggestedName,
+      },
+    });
+  }
+
+  onSubmit(): void {
+    console.log(this.form.value);
+    this.submitted = true;
+    this.user.username = this.form.value.userData.username;
+    this.user.email = this.form.value.userData.email;
+    this.user.secret = this.form.value.secret;
+    this.user.answer = this.form.value.answer;
+    this.user.gender = this.form.value.gender;
+    this.form.reset();
   }
 }
