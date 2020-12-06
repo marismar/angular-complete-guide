87d6af07101fb33700dcf638eb68e97960707d37
Marismar Costa <cmarismar@gmail.com>
2020-12-06 07:43:42 -0300

    handling template driven forms in angular

--- template-driven-form/src/app/app.component.ts
+++ template-driven-form/src/app/app.component.ts
@@ -9,57 +9,48 @@ import { NgForm } from '@angular/forms';
 export class AppComponent {
   title = 'template-driven-form';
 
-  // Lecture
-  // @ViewChild('form') form: NgForm;
-  // defaultOption = 'pet';
-  // answer = '';
-  // genders = ['male', 'female'];
-  // submitted = false;
-  // user = {
-  //   username: '',
-  //   email: '',
-  //   secret: '',
-  //   answer: '',
-  //   gender: '',
-  // };
-
-  // suggestUserName(): void {
-  //   const suggestedName = 'superuser';
-  //   // setValue() overwrite all inputs
-  //   /* this.form.setValue({
-  //     userData: {
-  //       username: suggestedName,
-  //       email: '',
-  //     },
-  //     secret: 'pet',
-  //     question: '',
-  //     gender: 'female',
-  //   }); */
-
-  //   // patchValue() dont overwrite filled inputs, but only empty one
-  //   this.form.form.patchValue({
-  //     userData: {
-  //       username: suggestedName,
-  //     },
-  //   });
-  // }
-
-  // onSubmit(): void {
-  //   console.log(this.form.value);
-  //   this.submitted = true;
-  //   this.user.username = this.form.value.userData.username;
-  //   this.user.email = this.form.value.userData.email;
-  //   this.user.secret = this.form.value.secret;
-  //   this.user.answer = this.form.value.answer;
-  //   this.user.gender = this.form.value.gender;
-  //   this.form.reset();
-  // }
-
-  // Exercise
   @ViewChild('form') form: NgForm;
-  defaultOption = 'advanced';
+  defaultOption = 'pet';
+  answer = '';
+  genders = ['male', 'female'];
+  submitted = false;
+  user = {
+    username: '',
+    email: '',
+    secret: '',
+    answer: '',
+    gender: '',
+  };
+
+  suggestUserName(): void {
+    const suggestedName = 'superuser';
+    // setValue() overwrite all inputs
+    /* this.form.setValue({
+      userData: {
+        username: suggestedName,
+        email: '',
+      },
+      secret: 'pet',
+      question: '',
+      gender: 'female',
+    }); */
+
+    // patchValue() dont overwrite filled inputs, but only empty one
+    this.form.form.patchValue({
+      userData: {
+        username: suggestedName,
+      },
+    });
+  }
 
   onSubmit(): void {
-    console.log(this.form);
+    console.log(this.form.value);
+    this.submitted = true;
+    this.user.username = this.form.value.userData.username;
+    this.user.email = this.form.value.userData.email;
+    this.user.secret = this.form.value.secret;
+    this.user.answer = this.form.value.answer;
+    this.user.gender = this.form.value.gender;
+    this.form.reset();
   }
 }
