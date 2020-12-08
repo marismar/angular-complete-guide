786c1106908d2decc53288b963c732c263c4ff29
Marismar Costa <cmarismar@gmail.com>
2020-12-08 20:21:55 -0300

    recipe app: reactive form for edit recipes

--- recipe-app/src/app/shopping-list/shopping-list.service.ts
+++ recipe-app/src/app/shopping-list/shopping-list.service.ts
@@ -13,7 +13,7 @@ export class ShoppingListService {
 
   private ingredients: Ingredient[] = [
     new Ingredient('Apples', 5),
-    new Ingredient('Tomatoes', 10),
+    new Ingredient('Tomato', 10),
   ];
 
   constructor() {}
