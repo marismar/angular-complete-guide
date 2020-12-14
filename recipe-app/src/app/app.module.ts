d4dd8967e889e3d2065b164e211c2a7e9712c64a
Marismar Costa <cmarismar@gmail.com>
2020-12-14 03:34:56 -0300

    recipe app: dynamic components

--- recipe-app/src/app/app.module.ts
+++ recipe-app/src/app/app.module.ts
@@ -21,6 +21,7 @@ import { DataStorageService } from './shared/data-storage.service';
 import { AuthComponent } from './auth/auth.component';
 import { LoadingSpinnerComponent } from './shared/loading-spinner/loading-spinner.component';
 import { AuthInterceptorService } from './auth/auth-interceptor.service';
+import { AlertComponent } from './shared/alert/alert.component';
 
 @NgModule({
   declarations: [
@@ -37,6 +38,7 @@ import { AuthInterceptorService } from './auth/auth-interceptor.service';
     RecipeEditComponent,
     AuthComponent,
     LoadingSpinnerComponent,
+    AlertComponent,
   ],
   imports: [
     BrowserModule,
