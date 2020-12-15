5faf4dba71b2fd49c8745f4447c4d949ea8b0419
Marismar Costa <cmarismar@gmail.com>
2020-12-15 01:25:35 -0300

    recipe app: lazy loading

--- recipe-app/src/app/app.module.ts
+++ recipe-app/src/app/app.module.ts
@@ -21,7 +21,6 @@ import { AuthModule } from './auth/auth.module';
     ReactiveFormsModule,
     HttpClientModule,
     AppRoutingModule,
-    RecipesModule,
     ShoppingListModule,
     SharedModule,
     CoreModule,
